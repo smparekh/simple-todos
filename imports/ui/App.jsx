f03d32e74924ab327af6e452507b3dfb17182a3a
Shaishav Parekh <shaishavparekh@gmail.com>
2016-08-09 14:47:21 -0400

    Initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,77 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
+
+import Task from './Task.jsx';
+
+// App component - represents the whole app
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			hideCompleted: false,
+		};
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+		Meteor.call('tasks.insert', text);
+
+		ReactDOM.findDOMNode(this.refs.textInput).value = '';
+	}
+
+	toggleHideCompleted() {
+		this.setState({
+			hideCompleted: !this.state.hideCompleted,
+		});
+	}
+
+	renderTasks() {
+		let filteredTasks = this.props.tasks;
+		if (this.state.hideCompleted) {
+			filteredTasks = filteredTasks.filter(task => !task.checked);
+		}
+		return filteredTasks.map((task) => (
+			<Task key={task._id} task={task} />
+		));
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<header>
+					<h1>Todo: {this.props.incompleteCount} Tasks</h1>
+					<label className="hide-completed">
+						<input type="checkbox" readOnly checked={this.state.hideCompleted} onClick={this.toggleHideCompleted.bind(this)} />
+						Hide Completed Tasks
+					</label>
+					<form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+						<input type="text" ref="textInput" placeholder="Type to add new tasks" />
+					</form>	
+				</header>
+
+				<ul>
+					{this.renderTasks()}
+				</ul>
+			</div>
+		);
+	}
+}
+
+App.propTypes = {
+	tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+	Meteor.subscribe('tasks');
+
+	return {
+		tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+		incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
+	};
+}, App);
